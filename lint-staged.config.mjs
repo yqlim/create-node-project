673b23a50e45fff5d362d14fe86e816f60db786d
yqlim <23203360+yqlim@users.noreply.github.com>
2025-02-15 16:00:39 +0800

    chore: add lint related commands

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -5,6 +5,7 @@ export default {
   '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,mdx}': [
     'eslint --cache --fix --max-warnings 0',
     'prettier --write',
+    () => 'tsc --noEmit',
   ],
-  '*.{css,html,json,md,sh,xml,yml,yaml}': 'prettier --write',
+  '*.{css,html,json,md,sh,xml,yml,yaml}': ['cspell lint', 'prettier --write'],
 };

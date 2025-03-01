12973194ac7bf1c08cb696dd06ba792f1b882b3d
yqlim <23203360+yqlim@users.noreply.github.com>
2025-03-01 19:39:08 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -45,6 +45,18 @@ export default tsEslint.config(
         },
       ],
       '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
       '@typescript-eslint/restrict-template-expressions': [
         'error',
         {
